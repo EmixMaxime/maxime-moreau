6b07d19282118a2cb9651543493451435be572c4
mx <emixmaxime@outlook.fr>
2019-05-19 16:53:03 +0200

    add(extend theme)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          900: 'hsl(202, 99%, 14%)'
+        },
+
+        mxblue: {
+          300: 'hsl(182, 81%, 34%)',
+          200: 'hsl(172, 76%, 48%)'
+        }
+      }
+    }
+  }
+}
